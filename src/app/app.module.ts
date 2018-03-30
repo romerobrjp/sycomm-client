da3c3064e2574ea92cdee4f3b4f7ee7e2eae72d3
Romero Medeiros <romeromedeiros5@gmail.com>
2018-03-30 11:09:30 -0300

    Configuração inicial do Admin LTE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { AsideComponent } from './shared/aside/aside.component';
+import { ContentComponent } from './shared/content/content.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ControlSidebarComponent } from './shared/control-sidebar/control-sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    ContentComponent,
+    FooterComponent,
+    ControlSidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/user.ts
@@ -0,0 +1,30 @@
+export class User {
+  public constructor(
+    public registration: number,
+    public name: string,
+    public surname: string,
+    public nickname: string,
+    public email: string,
+    public password: string,
+    public password_confirmation: string,
+    public cpf: string,
+    public landline: string,
+    public cellphone: string,
+    public whatsapp: string,
+    public simple_address: string,
+    public type: string,
+    public organization_id: number,
+    public role_id: number,
+    public address_id: number,
+    public created_at: Date,
+    public encrypted_password: string,
+    public reset_password_token: string,
+    public reset_password_sent_at: Date,
+    public sign_in_count: number,
+    public current_sign_in_at: Date,
+    public last_sign_in_at: Date,
+    public current_sign_in_ip: string,
+    public last_sign_in_ip: string,
+    public auth_token: string
+  ) {}
+}
