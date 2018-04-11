6c378ce553496282abf4463a7432b3c973d96560
Romero Medeiros <romeromedeiros5@gmail.com>
2018-04-10 21:54:52 -0300

    Fixing to Angular 5 way

--- src/app/organizations/shared/organization.service.ts
+++ src/app/organizations/shared/organization.service.ts
@@ -1,32 +1,26 @@
 import { Injectable } from '@angular/core';
-import {Headers, Http, Response} from '@angular/http';
-import {Observable} from 'rxjs/Observable';
-import {Organization} from './organization';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Organization } from './organization';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable()
 export class OrganizationService {
   public baseUrl = 'http://api.sycomm.com:3000/organizations';
-  private headers = new Headers({'Content-Type': 'application/json', 'Accept': 'application/vnd.sycomm.v1'});
+  private headers = new HttpHeaders({'Content-Type': 'application/json', 'Accept': 'application/vnd.sycomm.v1'});
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   public getAll(): Observable<Organization[]> {
-    return this.http.get(`${this.baseUrl}`, {headers: this.headers})
-      .catch(this.handleErrors)
-      .map((response: Response) => this.responseToOrganizations(response));
+    return this.http.get<Organization[]>(this.baseUrl, {headers: this.headers})
+      .pipe(
+        tap(users => console.log(users)),
+        catchError(this.handleErrors)
+      );
   }
 
   private handleErrors(error: Response) {
     console.error('Erro em OrganizationService: ' + error);
     return Observable.throw(error);
-  }
-
-  private responseToOrganizations(response: Response): Array<Organization> {
-    const organizations: Organization[] = [];
-
-    response.json().forEach(item => {
-      organizations.push(item as Organization);
-    });
-    return organizations;
   }
 }
--- src/app/roles/shared/role.service.ts
+++ src/app/roles/shared/role.service.ts
@@ -1,32 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http, Response } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Role } from './role';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable()
 export class RoleService {
   public baseUrl = 'http://api.sycomm.com:3000/roles';
-  private headers = new Headers({'Content-Type': 'application/json', 'Accept': 'application/vnd.sycomm.v1'});
+  private headers = new HttpHeaders({'Content-Type': 'application/json', 'Accept': 'application/vnd.sycomm.v1'});
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   public getAll(): Observable<Role[]> {
-    return this.http.get(`${this.baseUrl}`, {headers: this.headers})
-      .catch(this.handleErrors)
-      .map((response: Response) => this.responseToRoles(response));
+    return this.http.get<Role[]>(this.baseUrl, {headers: this.headers})
+      .pipe(
+        tap(users => console.log(users)),
+        catchError(this.handleErrors)
+      );
   }
 
   private handleErrors(error: Response) {
     console.error('Erro em RoleService: ' + error);
     return Observable.throw(error);
-  }
-
-  private responseToRoles(response: Response): Array<Role> {
-    const roles: Role[] = [];
-
-    response.json().forEach(item => {
-      roles.push(item as Role);
-    });
-    return roles;
   }
 }
