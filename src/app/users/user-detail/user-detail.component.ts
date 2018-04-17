3ba7309aeedde2eaed13ea271ec8aa5ea34f0848
Romero Medeiros <romeromedeiros5@gmail.com>
2018-04-16 21:05:36 -0300

    Form validation (required) with FormUtils

--- src/app/users/user-detail/user-detail.component.ts
+++ src/app/users/user-detail/user-detail.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnChanges, SimpleChanges} from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { Location, NgIf } from '@angular/common';
+import { Location } from '@angular/common';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 import { User } from '../shared/user.model';
@@ -10,11 +10,12 @@ import { RoleService } from '../../roles/shared/role.service';
 import { Organization } from '../../organizations/shared/organization.model';
 import { OrganizationService } from '../../organizations/shared/organization.service';
 
-import {MessageService} from 'primeng/components/common/messageservice';
-import {Message} from 'primeng/components/common/api';
+import { MessageService } from 'primeng/components/common/messageservice';
+import { Message } from 'primeng/components/common/api';
+
+import { FormUtils} from '../../shared/form-utils';
 
 import * as cpf from '@fnando/cpf'; // import the whole library
-import {isValid as isValidCpf} from '@fnando/cpf'; // import just one function
 
 @Component({
   selector: 'app-user',
@@ -22,12 +23,14 @@ import {isValid as isValidCpf} from '@fnando/cpf'; // import just one function
   styleUrls: ['./user-detail.component.css']
 })
 
-export class UserDetailComponent implements OnInit {
+export class UserDetailComponent implements OnInit, OnChanges {
   user: User;
   roles: Role[];
   organizations: Organization[];
   form: FormGroup;
+  formUtils: FormUtils;
   msgs: Message[] = [];
+  registrationMask = [/\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/];
   cpfMask = [/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/];
   phoneMask = ['(', /\d/, /\d/, ')', ' ', /\d/, '-', /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
 
@@ -41,16 +44,16 @@ export class UserDetailComponent implements OnInit {
   ) {
     this.user = new User(
       null,
-      null,
-      null,
-      null,
-      null,
-      null,
-      null,
-      null,
-      null,
-      null,
-      null,
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
       'Customer',
       null,
       null,
@@ -69,16 +72,18 @@ export class UserDetailComponent implements OnInit {
 
     this.form = this.formBuilder.group({
       name: [null, [Validators.required]],
-      email: [null, Validators.required],
+      email: [null],
       registration: [null, Validators.required],
-      cpf: [null, Validators.required],
+      cpf: [null, [Validators.required]],
       landline: [null],
-      cellphone: [null, Validators.required],
+      cellphone: [null, [Validators.required]],
       whatsapp: [null],
       simple_address: [null],
       role_id: [null, Validators.required],
       organization_id: [null, Validators.required]
     });
+
+    this.formUtils = new FormUtils(this.form);
   }
 
   ngOnInit() {
@@ -120,14 +125,33 @@ export class UserDetailComponent implements OnInit {
   update() {
     this.applyFormValues();
 
+    let errorFound = false;
+
     if (!cpf.isValid(this.user.cpf)) {
-      this.messageService.add({severity: 'warn', summary: 'Atenção', detail: 'CPF inválido.'})
-      return false;
+      this.messageService.add({severity: 'error', summary: 'Erro', detail: 'CPF inválido.'});
+      errorFound = true;
     }
 
+    if (this.user.landline && this.user.landline.length !== 11) {
+      this.messageService.add({severity: 'error', summary: 'Erro', detail: 'Telefone fixo precisa ter 11 dígitos.'});
+      errorFound = true;
+    }
+
+    if (this.user.landline && this.user.cellphone.length !== 11) {
+      this.messageService.add({severity: 'error', summary: 'Erro', detail: 'Celular precisa ter 11 dígitos.'});
+      errorFound = true;
+    }
+
+    if (this.user.whatsapp && this.user.whatsapp.length !== 11) {
+      this.messageService.add({severity: 'error', summary: 'Erro', detail: 'WhatsApp precisa ter 11 dígitos.'});
+      errorFound = true;
+    }
+
+    if (errorFound) { return false; };
+
     this.userService.update(this.user).subscribe(
-      () => this.messageService.add({severity: 'success', summary: 'Sucesso', detail: 'Usuário atualizado!'}),
-      (error) => this.messageService.add({severity: 'error', summary: 'Erro', detail: 'Ocorreu um erro inesperado ao atualizar o usuário.'})
+      (response) => this.messageService.add({severity: 'success', summary: 'Sucesso', detail: 'Usuário atualizado!'}),
+      (errorRseponse) => this.messageService.add({severity: 'error', summary: 'Erro', detail: errorRseponse.error.errors.cellphone[0]})
     );
   }
 
@@ -149,15 +173,19 @@ export class UserDetailComponent implements OnInit {
   }
 
   private applyFormValues() {
-    this.user.name = this.form.get('name').value;
-    this.user.email = this.form.get('email').value;
-    this.user.registration = this.form.get('registration').value;
-    this.user.cpf = cpf.strip(this.form.get('cpf').value);
-    this.user.landline = this.form.get('landline').value;
-    this.user.cellphone = this.form.get('cellphone').value;
-    this.user.whatsapp = this.form.get('whatsapp').value;
-    this.user.simple_address = this.form.get('simple_address').value;
-    this.user.role_id = this.form.get('role_id').value;
-    this.user.organization_id = this.form.get('organization_id').value;
+    if (this.form.get('name').value) { this.user.name = this.form.get('name').value; }
+    if (this.form.get('email').value) { this.user.email = this.form.get('email').value; }
+    if (this.form.get('registration').value) { this.user.registration = this.form.get('registration').value; }
+    if (this.form.get('cpf').value) { this.user.cpf = cpf.strip(this.form.get('cpf').value).match(/\d+/g).join([]); }
+    if (this.form.get('landline').value) { this.user.landline = this.form.get('landline').value.match(/\d+/g).join([]); }
+    if (this.form.get('cellphone').value) { this.user.cellphone = this.form.get('cellphone').value.match(/\d+/g).join([]); }
+    if (this.form.get('whatsapp').value) { this.user.whatsapp = this.form.get('whatsapp').value.match(/\d+/g).join([]); }
+    if (this.form.get('simple_address').value) { this.user.simple_address = this.form.get('simple_address').value; }
+    if (this.form.get('role_id').value) { this.user.role_id = this.form.get('role_id').value; }
+    if (this.form.get('organization_id').value) { this.user.organization_id = this.form.get('organization_id').value; }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
   }
 }
