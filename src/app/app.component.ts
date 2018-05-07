36f07797e343db4cba4b4c9dd412904aaa3c4eff
Romero Medeiros <romeromedeiros5@gmail.com>
2018-05-07 19:07:13 -0300

    Configurando URL base para requisições à API rails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Angular2TokenService } from 'angular2-token';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public constructor(private tokenService: Angular2TokenService) {
+    this.tokenService.init({
+      apiBase: 'http://api.sycomm.com:3000'
+    });
+  }
 }
