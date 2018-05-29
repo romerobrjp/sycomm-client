1af7eb2fe91f6f59bab5af43a017d9372c665cfa
Romero Medeiros <romeromedeiros5@gmail.com>
2018-05-28 23:08:21 -0300

    Terminando edição de campos em profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,10 @@
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/components/common/messageservice';
+import { UserService } from './../users/shared/user.service';
+import { FormUtils } from './../shared/form-utils';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import * as cpf_lib from '@fnando/cpf';
 
 @Component({
   selector: 'app-profile',
@@ -7,19 +13,115 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   currentUser;
+  form: FormGroup;
+  formUtils: FormUtils;
   userTypesDictionary = {
     'Admin' : 'Admin',
     'Employee' : 'Funcionário',
     'Customer' : 'Cliente'
   };
+  attributesDictionary = {
+    'registration' : 'Matrícula',
+    'name' : 'Nome',
+    'email' : 'E-mail',
+    'password' : 'Senha',
+    'password_confirmation' : 'Confirmação de Senha',
+    'cpf' : 'CPF',
+    'landline' : 'Telefone fixo',
+    'cellphone' : 'Celular',
+    'whatsapp' : 'WhatsApp',
+    'simples_adress' : 'Endereço',
+    'type' : 'Tipo de usuário',
+    'organization' : 'Organização',
+    'role' : 'Cargo'
+  };
 
-  constructor() { }
+  constructor(private userService: UserService, private formBuilder: FormBuilder, private messageService: MessageService, private router: Router) {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: [''],
+      registration: ['', Validators.required],
+      cpf: ['', [Validators.required]],
+      landline: [''],
+      cellphone: ['', [Validators.required]],
+      whatsapp: [''],
+      simple_address: ['']
+    });
 
-  ngOnInit() {
-    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.formUtils = new FormUtils(this.form);
   }
 
-  getCurrentUser() {
-    // return JSON.parse(localStorage.getItem('currentUser'));
+  ngOnInit() {
+    this.userService.getById(this.getCurrentUserFromLocalStorage().id).subscribe(
+      user => {
+        this.currentUser = user;
+        this.form.patchValue(this.currentUser);
+      }
+    );
+  }
+
+  getCurrentUserFromLocalStorage() {
+    return JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  isAdmin(): boolean {
+    return this.currentUser.type === 'Admin';
+  }
+
+  isEmployee(): boolean {
+    return this.currentUser.type === 'Employee';
+  }
+
+  isCustomer(): boolean {
+    return this.currentUser.type === 'Customer';
+  }
+
+  update(): void {
+    this.messageService.clear();
+    this.applyFormValues();
+
+    this.userService.update(this.currentUser).subscribe(
+      (response) => {
+        this.messageService.add({severity: 'success', summary: 'Sucesso', detail: 'Perfil atualizado!'});
+        this.currentUser = this.userService.getById(this.currentUser.id).subscribe(
+          user => this.currentUser = user
+        );
+      },
+      (errorRseponse) => {
+        for (const [key, value] of Object.entries(errorRseponse.error.errors)) {
+          for (const [errorKey, errorMessage] of Object.entries(value)) {
+            this.messageService.add({
+              key: 'user_detail_messages',
+              severity: 'error',
+              summary: this.attributesDictionary[key],
+              detail: errorMessage
+            });
+          }
+        }
+      }
+    );
+  }
+
+  private applyFormValues() {
+    this.currentUser.name = this.form.get('name').value;
+    this.currentUser.email = this.form.get('email').value;
+    this.currentUser.registration = this.form.get('registration').value;
+    if (this.form.get('cpf').value) {
+      this.currentUser.cpf = cpf_lib.strip(this.form.get('cpf').value);
+    } else {
+      this.currentUser.cpf = '';
+    }
+    this.currentUser.landline = this.stripPhoneNumbers(this.form.get('landline').value);
+    this.currentUser.cellphone = this.stripPhoneNumbers(this.form.get('cellphone').value);
+    this.currentUser.whatsapp = this.stripPhoneNumbers(this.form.get('whatsapp').value);
+    this.currentUser.simple_address = this.form.get('simple_address').value;
+  }
+
+  private stripPhoneNumbers(phoneNumber: string) {
+    if (phoneNumber) {
+      return phoneNumber.replace(/\D/g, '');
+    } else {
+      return '';
+    }
   }
 }
