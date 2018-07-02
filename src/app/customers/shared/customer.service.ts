cdec7aeb4ea752051712e8fc1c5ec5ceb1bdcd09
Romero Medeiros <romeromedeiros5@gmail.com>
2018-07-02 15:08:19 -0300

    Corrigindo URl base

--- src/app/customers/shared/customer.service.ts
+++ src/app/customers/shared/customer.service.ts
@@ -6,37 +6,37 @@ import { Customer } from './customer.model';
 
 @Injectable()
 export class CustomerService {
-  public baseUrl = 'http://api.sycomm.com:3000/customers';
+  public resourceUrl = '/customers';
   private headers = new HttpHeaders({'Content-Type': 'application/json', 'Accept': 'application/vnd.sycomm.v1'});
 
   constructor(private http: HttpClient) { }
 
   public listPaginated(page_number: number, per_page: number): Observable<Customer[]> {
-    const url = `${this.baseUrl}/list_paginated?page_number=${page_number}&per_page=${per_page}`;
+    const url = `${this.resourceUrl}/list_paginated?page_number=${page_number}&per_page=${per_page}`;
 
     return this.http.get<Customer[]>(url);
   }
 
   public getById(id: number): Observable<Customer> {
-    const url = `${this.baseUrl}/${id}`;
+    const url = `${this.resourceUrl}/${id}`;
 
     return this.http.get<Customer>(url);
   }
 
   public create(customer: Customer): Observable<Customer> {
-    const url = `${this.baseUrl}`;
+    const url = `${this.resourceUrl}`;
 
     return this.http.post<Customer>(url, customer, { headers: this.headers });
   }
 
   public update(customer: Customer): Observable<Customer> {
-    const url = `${this.baseUrl}/${customer.id}`;
+    const url = `${this.resourceUrl}/${customer.id}`;
 
     return this.http.put<Customer>(url, customer, { headers: this.headers });
   }
 
   public delete(id: number): Observable<{}  > {
-    const url = `${this.baseUrl}/${id}`;
+    const url = `${this.resourceUrl}/${id}`;
 
     return this.http.delete(url, { headers: this.headers});
   }
