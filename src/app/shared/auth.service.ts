f0c6f89d59381eded8a89105e147c39b53cb072b
Romero Medeiros <romeromedeiros5@gmail.com>
2018-07-02 15:07:55 -0300

    Utilizando o validateToken para pegar o currentUser

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -2,14 +2,14 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {User} from '../users/shared/user.model';
 import {TokenService} from './token.service';
-import {tap} from 'rxjs/operators';
+import {ErrorHandlerService} from './error-handler.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private tokenService: TokenService) {}
+  constructor(private tokenService: TokenService, private errorHandlerService: ErrorHandlerService) {}
 
   signUp(user: User): Observable<Response> {
-    return this.tokenService.registerAccount(user as any).catch(this.handleErrors);
+    return this.tokenService.registerAccount(user as any).catch(ErrorHandlerService.handleResponseErrors);
   }
 
   signIn(uid: string, password: string): Observable<Response> {
@@ -18,33 +18,18 @@ export class AuthService {
       password: password
     };
 
-    return this.tokenService.signIn(signInData).pipe(
-      tap(
-        responseSuccess => {
-          const currentUser = JSON.parse(responseSuccess['_body']).data;
-          localStorage.setItem('currentUser', JSON.stringify(currentUser));
-        },
-        responseError => {
-          this.handleErrors(responseError);
-        }
-      )
-    ).catch(this.handleErrors);
+    return this.tokenService.signIn(signInData).catch(ErrorHandlerService.handleResponseErrors);
   }
 
   signOut(): Observable<Response> {
-    return this.tokenService.signOut().catch(this.handleErrors);
+    return this.tokenService.signOut().catch(ErrorHandlerService.handleResponseErrors);
   }
 
   userSignedIn(): boolean {
     return this.tokenService.userSignedIn();
   }
 
-  handleErrors(error: Response) {
-    console.log('AuthService.handleErrors: ', error);
-    return Observable.throw(error);
-  }
-
-  getCurrentUser(): User {
+  getCurrentUser() {
     return JSON.parse(localStorage.getItem('currentUser'));
   }
 
@@ -62,5 +47,16 @@ export class AuthService {
 
   updateCurrentUser(u: User): void {
     localStorage.setItem('currentUser', JSON.stringify(u));
+  }
+
+  currentUser(): Observable<User> {
+    return this.tokenService.validateToken().map(
+      res => res.json()['data'] as User
+    );
+  }
+
+  handleErrors(error: Response) {
+    console.log('AuthService.handleErrors: ', error);
+    return Observable.throw(error);
   }
 }
