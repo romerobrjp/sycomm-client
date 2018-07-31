0af7d72de69ad4a06543fc71557dcc56aaff9114
Romero Medeiros <romeromedeiros5@gmail.com>
2018-07-30 23:44:45 -0300

    Adicionando sortOrderDictionary

--- src/app/shared/general-utils.ts
+++ src/app/shared/general-utils.ts
@@ -1,4 +1,6 @@
 export class GeneralUtils {
+  static sortOrderDictionary = new Map<number, string>().set(1, 'asc').set(-1, 'desc');
+
   static stripPhoneNumbers(phoneNumber: string) {
     if (phoneNumber) {
       return phoneNumber.replace(/\D/g, '');
