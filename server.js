268d29a2c194c99c716dda83509c8af780522572
Romero Medeiros <romeromedeiros5@gmail.com>
2018-08-12 12:11:44 -0300

    Ajustando deploy de acordo com https://medium.com/@babubhai/how-to-deploy-angular-6-app-to-heroku-52b73ac7a3aa

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require("express");
+//Install express server
+const express = require('express');
+const path = require('path');
+
 const app = express();
-const path = require("path");
 
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/sycomm-client'));
 
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname,'/dist/sycomm-client/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
