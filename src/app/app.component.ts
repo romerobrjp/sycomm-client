2111c433faa686e54392c5053e28e611bd82383f
Romero Medeiros <romeromedeiros5@gmail.com>
2018-08-13 21:42:55 -0300

    URL da API em Dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { AuthService } from './shared/auth.service';
 export class AppComponent {
   constructor(private tokenService: TokenService, private authService: AuthService) {
     this.tokenService.init({
-      apiBase: 'https://sycomm-api.herokuapp.com',
+      apiBase: 'http://api.sycomm.com:3000',
       globalOptions: {
         headers: {
           'Content-Type': 'application/json',
