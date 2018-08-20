155915912b177b3d759316e2e573ca69d8311c7c
Romero Meireles <romero.mfm@gmail.com>
2018-08-20 10:42:58 -0300

    Corrigindo AgendaService para trabalhar com HttpClient; Adicionando #deleteSelected.

--- src/app/agendas/shared/agenda.service.ts
+++ src/app/agendas/shared/agenda.service.ts
@@ -14,51 +14,44 @@ export class AgendaService {
   listLastEmployeeAgendas(userId, quant): Observable<Agenda[]> {
     const url = `${this.urlResource}`;
 
-    return this.http.get(`${url}/list_last_emplyee_agendas?employee_id=${userId}&quant=${quant}`).pipe(
+    return this.http.get<Agenda[]>(`${url}/list_last_emplyee_agendas?employee_id=${userId}&quant=${quant}`).pipe(
       catchError(this.handleErrors),
-      map((response: Response) => this.responseToModels(response)),
     );
   }
 
-  listAllPaginated(page_number: number, per_page: number): Observable<any> {
+  listAllPaginated(page_number: number, per_page: number): Observable<Object> {
     const url = `${this.urlResource}/list_all_paginated?page_number=${page_number}&per_page=${per_page}`;
 
-    return this.http.get(url).pipe(catchError(this.handleErrors));
+    return this.http.get<Object>(url).pipe(catchError(this.handleErrors));
   }
 
-  listEmployeeAgendasPaginated(employeeId: number, page_number: number, per_page: number): Observable<any> {
+  listEmployeeAgendasPaginated(employeeId: number, page_number: number, per_page: number): Observable<Object> {
     const url = `${this.urlResource}/list_employee_agendas_paginated?employee_id=${employeeId}&page_number=${page_number}&per_page=${per_page}`;
 
-    return this.http.get(url).pipe(catchError(this.handleErrors));
+    return this.http.get<Object>(url).pipe(catchError(this.handleErrors));
   }
 
   getById(id: number): Observable<Agenda> {
     const url = `${this.urlResource}/${id}`;
 
-    // return this.http.get<Agenda>(url).pipe(
-    //   catchError(this.handleErrors),
-    //   map((response: Response) => this.responseToModel(response))
-    // );
-    return this.http.get<Agenda>(url);
+    return this.http.get<Agenda>(url).pipe(
+      catchError(this.handleErrors),
+    );
   }
 
   create(entity: Agenda): Observable<Agenda> {
     const url = `${this.urlResource}`;
 
-    // return this.http.post(url, entity).pipe(
-    //   map((response: Response) => this.responseToModel(response)),
-    //   catchError(this.handleErrors),
-    // );
-
-    return this.http.post<Agenda>(url, entity);
+    return this.http.post<Agenda>(url, entity).pipe(
+      catchError(this.handleErrors),
+    );
   }
 
   update(entity: Agenda): Observable<Agenda> {
     const url = `${this.urlResource}/${entity.id}`;
 
-    return this.http.put(url, entity).pipe(
+    return this.http.put<Agenda>(url, entity).pipe(
       catchError(this.handleErrors),
-      map((response: Response) => this.responseToModel(response))
     );
   }
 
@@ -71,66 +64,16 @@ export class AgendaService {
     );
   }
 
-  deleteAgendas(agendasIDs: number[]) {
-    // const url = `${this.urlResource}/delete-many`;
-    // const params = {
-    //   'agendas_ids': agendasIDs.toString()
-    // };
-    // const requestOptions = new RequestOptions({ body: params });
-    //
-    // return this.http.delete(url, {body: requestOptions}).pipe(
-    //   catchError(this.handleErrors),
-    //   map(() => null),
-    // );
-  }
+  deleteAgendas(agendasIDs: number[]): Observable<null> {
+    const url = `${this.urlResource}/delete-many`;
+    const params = {
+      'agendas_ids': agendasIDs.toString()
+    };
 
-  private responseToModel(response: Response): Agenda {
-    const jsonEntity = response.json();
-
-    const customersCpf: Array<string> = jsonEntity['customers'].map(c => c.cpf);
-
-    return new Agenda(
-      jsonEntity['id'],
-      jsonEntity['name'],
-      jsonEntity['start_date'],
-      jsonEntity['end_date'],
-      jsonEntity['employee_id'],
-      jsonEntity['employee'],
-      jsonEntity['customers'],
-      customersCpf,
-      jsonEntity['created_at'],
-      jsonEntity['updated_at'],
-      jsonEntity['activities'],
-      jsonEntity['open_activities_count'],
+    return this.http.delete(url, { params: params }).pipe(
+      catchError(this.handleErrors),
+      map(() => null),
     );
-  }
-
-  private responseToModels(response: Response): Array<Agenda> {
-    const collection = response.json()['data'] as Array<any>;
-    const items: Agenda[] = [];
-
-    collection.forEach(jsonEntity => {
-      const customersCpf: Array<string> = jsonEntity['customers'].map(c => c.cpf);
-
-      const item = new Agenda(
-        jsonEntity['id'],
-        jsonEntity['name'],
-        jsonEntity['start_date'],
-        jsonEntity['end_date'],
-        jsonEntity['employee_id'],
-        jsonEntity['employee'],
-        jsonEntity['customers'],
-        customersCpf,
-        jsonEntity['created_at'],
-        jsonEntity['updated_at'],
-        jsonEntity['activities'],
-        jsonEntity['open_activities_count'],
-      );
-
-      items.push(item);
-    });
-
-    return items;
   }
 
   private handleErrors(error: Response) {
